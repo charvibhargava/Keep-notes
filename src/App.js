2e2799cf73b4cbbb70f7beea7aec7ecf75854236
charvi bhargava <bhargavacharviiii@gmail.com>
2022-10-26 16:09:38 +0530

    Sidebar Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NoteContainer from './components/NoteContainer/NoteContainer';
+import Sidebar from './components/Sidebar/Sidebar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Sidebar/>
       <NoteContainer/>
     </div>
   );
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import plusIcon from '../../assets/transparent-black-icon.png'
+import './Sidebar.css'
+
+function Sidebar() {
+    const colors =["#fe9b72", "#fec971", "#00d4fe", "#b693fd", "#e4ee91"]
+    const [listOpen, setListOpen] = useState(false);
+  return (
+    <div className='sidebar'>
+      <img src={plusIcon} alt="Add" onClick={()=> setListOpen(!listOpen)}/>
+      <ul className={`sidebar_list ${listOpen? "sidebar_list_active":""}`}>
+        {
+            colors.map((item, index)=>(
+            <li key= {index} 
+            className='sidebar_list_item' 
+            style={{backgroundColor:item}}
+            />
+            ))
+        }
+        
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
