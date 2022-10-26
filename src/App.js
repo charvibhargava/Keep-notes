8e4ad18256b5c233b80c2a2cb633c711bb34489f
charvi bhargava <bhargavacharviiii@gmail.com>
2022-10-26 15:00:57 +0530

    Note section completed.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import NoteContainer from './components/NoteContainer/NoteContainer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Working</h1>
+      <NoteContainer/>
     </div>
   );
 }
---
+++ src/components/NoteContainer/NoteContainer.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import Note from '../Note/Note';
+import './NoteContainer.css';
+
+function NoteContainer() {
+  return (
+    <div className='note-container'>
+      <h2>Notes</h2>
+      <div className="note-container_notes custom-scroll">
+
+        <Note note={{
+          text: "adffff",
+          time: "4:32",
+          color: "yellow"
+        }}/>
+        <Note note={{
+          text: "adffff",
+          time: "4:32",
+          color: "yellow"
+        }}/>
+        <Note note={{
+          text: "adffff",
+          time: "4:32",
+          color: "yellow"
+        }}/>
+        <Note note={{
+          text: "adffff",
+          time: "4:32",
+          color: "yellow"
+        }}/>
+        <Note note={{
+          text: "adffff",
+          time: "4:32",
+          color: "yellow"
+        }}/>
+        <Note note={{
+          text: "adffff",
+          time: "4:32",
+          color: "yellow"
+        }}/>
+        <Note note={{
+          text: "adffff",
+          time: "4:32",
+          color: "yellow"
+        }}/>
+        <Note note={{
+          text: "adffff",
+          time: "4:32",
+          color: "yellow"
+        }}/>
+        <Note note={{
+          text: "adffff",
+          time: "4:32",
+          color: "yellow"
+        }}/>
+        <Note note={{
+          text: "adffff",
+          time: "4:32",
+          color: "yellow"
+        }}/>
+        <Note note={{
+          text: "adffff",
+          time: "4:32",
+          color: "yellow"
+        }}/>
+        <Note note={{
+          text: "adffff",
+          time: "4:32",
+          color: "yellow"
+        }}/>
+        <Note note={{
+          text: "adffff",
+          time: "4:32",
+          color: "yellow"
+        }}/>
+        <Note note={{
+          text: "adffff",
+          time: "4:32",
+          color: "yellow"
+        }}/>
+        <Note note={{
+          text: "adffff",
+          time: "4:32",
+          color: "yellow"
+        }}/>
+        <Note note={{
+          text: "adffff",
+          time: "4:32",
+          color: "yellow"
+        }}/>
+        <Note note={{
+          text: "adffff",
+          time: "4:32",
+          color: "yellow"
+        }}/>
+        <Note note={{
+          text: "adffff",
+          time: "4:32",
+          color: "yellow"
+        }}/>
+        <Note note={{
+          text: "adffff",
+          time: "4:32",
+          color: "yellow"
+        }}/>
+        <Note note={{
+          text: "adffff",
+          time: "4:32",
+          color: "yellow"
+        }}/>
+      </div>
+    </div>
+  )
+}
+
+export default NoteContainer
